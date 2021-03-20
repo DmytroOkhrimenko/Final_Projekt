444520857e689840267bbecf4f39427fd090d189
DmytroOkhrimenko <75792095+DmytroOkhrimenko@users.noreply.github.com>
2021-03-20 14:02:38 +0200

    Update run_g03.sh

--- run_g03.sh
+++ run_g03.sh
@@ -1,43 +1,13 @@
-#!/bin/bash
+#!/bin/bash 
 
+# set up environmental variables 
+export GAUSS_SCRDIR=/home/ubuntu/scratch
+export GAUSS_EXEDIR=/home/ubuntu/gaussian/g03
+export g03root=/home/ubuntu/gaussian
+export PATH=$PATH:$GAUSS_EXEDIR
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GAUSS_EXEDIR
 
-TASK_DIR=/home/ubuntu/amasinggrave
-TOOL=/home/ubuntu/tools
-
-
-
-count=$( ls $TASK_DIR | wc -l )
-if(( count==0 )); then
-    s3fs amasinggrave ${TASK_DIR} -o passwd_file=${HOME}/.passwd-s3fs
-    echo "Bucket is mounted"
-else
-    echo "Bucket is already mounted"
-fi
-
-
-ls ${TASK_DIR}/*.txt  >/dev/null 2>&1
-
-if [ "$?" -gt 0  ]; then
- echo "No inputs .txt  files in backet. Exit"
- exit 2
-fi
-
-
-
-LOG=/home/ubuntu/log_file.txt
-if [ ! -f $LOG ] ; then
-    touch $LOG
-fi
-
-
-cp ${TOOL}/parse.py ${WORK_DIR}/.
-cp ${TOOL}/run_g03.sh ${WORK_DIR}/.
-chmod +x ${WORK_DIR}/run_g03.sh 
-
-
-
-
-
+g03 $1
 
 
 
